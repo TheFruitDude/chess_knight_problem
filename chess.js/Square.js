72cc04d9fa5fb50732caa4d5b60aea9c469681a5
TheFruitDude <36520284+TheFruitDude@users.noreply.github.com>
2020-06-14 15:01:17 +0200

    Add files via upload

--- chess.js/Square.js
+++ chess.js/Square.js
@@ -1,6 +1,7 @@
-function Square(_x, _y) {
+function Square(_x, _y, _i) {
   this.x = _x;
   this.y = _y;
+  this.i = _i;
 
   this.visited = false;
 
@@ -11,5 +12,19 @@ function Square(_x, _y) {
     noFill();
     rect(this.x, this.y, 50, 50);
     pop();
+  }
+  this.index = function() {
+    // it's position (index)
+    return this.i
+  }
+
+  this.write = function() {
+    // simply writes the cell number in his field
+
+    textSize(16)
+    fill('red')
+    text(this.index.toString(), this.x+15, this.y+15, 50, 50)
+
+// counting from 0 to 63 from left to right obvisouly
   }
 }
--- chess.js/sketch.js
+++ chess.js/sketch.js
@@ -22,10 +22,12 @@ function setup() {
   createCanvas(400, 400);
 
   // Create the squares
+  var index = 0;
   for (y = 0; y < height; y += 50) {
     for (x = 0; x < width; x += 50) {
-      square = new Square(x, y);
+      square = new Square(x, y, index);
       squares.push(square);
+    index++;
     }
   }
 
@@ -47,7 +49,13 @@ function draw() {
   drawBoard();
   let x = squares[p].x
   let y = squares[p].y
-  //console.log(squares[count])
+
+  squares.forEach( (el) => {
+    el.write()
+  //  console.log(el.index())
+  })
+
+
   //image(img, x, y)
   //image(img, p, q)
 }
@@ -55,11 +63,46 @@ function draw() {
 
 
 function allPossibleMoves(pos) {
+
   // check all available moves from a given position
   // input is this format: [x, y]
-console.log(one_two(8))
+  // input needs to be mapped first
+
+  // this function returns an array containing all possible coordinates where the knight can moves
+
+
+  //console.log(pos[0] + " " + pos[1] + "  translates to " + map_to_8(pos))
+  var a = map_to_8(pos) // 0 to 7
+  var p = a[0]
+  var q = a[1]
+
+  // all possible moves of the knight
+  const X = [2, 1, -1, -2, -2, -1, 1, 2]
+  const Y = [1, 2, 2, 1, -1, -2, -2, -1]
+  var possible_fields = []
+  for (let j = 0; j < 8; j++) {
+    // console.log("p + X[j]:" +  " " + p + " " + X[j])
+    // testing all moves:
+    var x = p + X[j]
+    var y = q + Y[j]
+
+    if (x >= 0 && y >= 0 && y < 8 && y < 8) {
+      // here check if visited
+
+      let arr = []
+      arr.push(x)
+      arr.push(y)
+      possible_fields.push(arr)
+    }
+  }
+  // we were here
+//console.log(p + " " + q + " can move to ")
+//console.log(possible_fields)
+
+
 
 }
+
 
 function square_with_fewest_onward_moves() {
   let arr = [] //array to return
@@ -73,7 +116,7 @@ function one_two(i) {
   let y = Math.floor(i / 8)
 
   let arr = []
-  arr.push(x,y)
+  arr.push(x, y)
   return arr
 }
 
@@ -81,8 +124,26 @@ function two_one(posX, posY) {
   //translates Array grid from 2d to 1d and returns zero based 1 dim
   posX++ // add one because zero-based index
   posY++
-  return ((posY-1) * 8 + (posX)) -1
+  return ((posY - 1) * 8 + (posX)) - 1
 
+}
+// translates from coordinates to 0-7
+function map_to_8(pos) {
+
+  x = map(pos[0], 0, 350, 0, 7)
+  y = map(pos[1], 0, 350, 0, 7)
+
+  if (pos[0] == 0) {
+    x = 0
+  }
+  if (pos[1] == 0) {
+    y = 0
+  }
+
+  let arr = []
+  arr.push(x)
+  arr.push(y)
+  return arr
 }
 
 // Function just for drawing the board
